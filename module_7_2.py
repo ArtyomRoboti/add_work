a72506eb180dced23b41ae48fbd5b30c06cbecc6
ArtyomRoboti <59234653+ArtyomRoboti@users.noreply.github.com>
2024-09-01 14:20:26 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,22 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+    number_col = 1
+    file = open(file_name, mode='w', encoding='utf-8')
+    for f in strings:
+        begin_file = file.tell()
+        file.write(f + '/n')
+        strings_positions[(number_col, begin_file)] = f
+        number_col += 1
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
